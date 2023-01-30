38b38a789a6dc22544e3b583039c5f67ebaeafe1
MikeTribbiani <mishachaban8@gmail.com>
2023-01-30 17:41:12 +0200

    files clearing

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    //baseUrl: "https://app.clickup.com",
-    excludeSpecPattern:
-      ("**/1-getting-started/*.js", "**/2-advanced-examples/*.js"),
+    excludeSpecPattern: "**/1-getting-started/*.js",
   },
 });
